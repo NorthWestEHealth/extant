689bda617ff49b1e03f3c252e35363629c9db972
mdehsjmt <james.mcgrath@manchester.ac.uk>
2017-08-14 15:25:04 +0100

    Removing stray deployment folder

--- source/Extant/Deployment/Scripts/utilities.js
+++
@@ -1,4 +0,0 @@
-﻿//see http://stackoverflow.com/questions/2094618/changing-name-attr-of-cloned-input-element-in-jquery-doesnt-work-in-ie6-7
-function setElementName(elems, name) {
-    $(elems).attr('name', name);
-}
